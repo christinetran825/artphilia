09e47957348e81c08077a0ffd2ca87ac117b5192
Christine Tran <christinetran@Christines-iMac.local>
2017-12-09 23:18:22 -0800

    added Sessions actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'welcome#home'
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
 end
