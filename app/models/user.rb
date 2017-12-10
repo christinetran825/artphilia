79603548bc0be1b2aa96ae822972a7e3616f848e
Christine Tran <christinetran@Christines-iMac.local>
2017-12-09 23:26:59 -0800

    added has secure password and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
 
+  has_secure_password
+
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
 
   #find or create by oauth user_id
   def self.set_user_from_omniauth(uid)
