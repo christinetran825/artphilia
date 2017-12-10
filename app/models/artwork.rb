06109369dc8d2cadaabcde9984ab40e50b9edbb4
Christine Tran <christinetran@Christines-iMac.local>
2017-12-10 00:26:46 -0800

    added associations and validations

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,2 +1,20 @@
 class Artwork < ApplicationRecord
+  belongs_to :artist
+  belongs_to :medium
+
+  before_validation :make_title_case
+
+  validates_presence :title, :exhibition, :user_owned, :signed, :original, :rating, :comments, :medium
+
+  def make_title_case
+    self.title = self.title.titlecase
+    self.exhibition = self.exhibition.titlecase
+  end
+
+  # def media_attributes=(medium_attributes)
+  #   medium_attributes.values.each do |medium_attribute|
+  #     medium = Medium.find_or_create_by(medium_attribute)
+  #     self.artwork_media.build(medium: medium)
+  #   end
+  # end
 end
