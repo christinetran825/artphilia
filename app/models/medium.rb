32630701037c55af33cf65ab48cddedb90e44412
Christine Tran <christinetran@Christines-iMac.local>
2017-12-10 00:36:27 -0800

    defined associations and validations

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,2 +1,11 @@
 class Medium < ApplicationRecord
+  has_many :artworks #medium's plural is media
+  has_many :artists, through: :artworks #medium's plural is media
+
+  before_validation :make_title_case
+  validates :name, presence: true
+
+  def make_title_case
+    self.name = self.name.titlecase
+  end
 end
