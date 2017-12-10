93a7ea51fdc0338341bb2661a14cdb59de18c181
Christine Tran <christinetran@Christines-iMac.local>
2017-12-10 00:22:06 -0800

    added Artworks attributes

--- db/migrate/20171210080726_create_artworks.rb
+++ db/migrate/20171210080726_create_artworks.rb
@@ -1,7 +1,15 @@
 class CreateArtworks < ActiveRecord::Migration[5.1]
   def change
     create_table :artworks do |t|
-
+      t.string :title
+      t.string :exhibition
+      t.string :medium
+      t.string :user_owned
+      t.string :signed
+      t.string :original
+      t.integer :rating
+      t.string :comments
+      
       t.timestamps
     end
   end
