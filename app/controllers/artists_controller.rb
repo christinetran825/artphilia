ad846a85ca606a8b0b5754899dec68fb7b20c242
Christine Tran <christinetran@Christines-iMac.local>
2017-12-10 00:02:11 -0800

    defined action routes

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,57 @@
 class ArtistsController < ApplicationController
+
+  before_action :set_artist, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @artists = Artist.all
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+    @artist.user = current_user
+    if @artist && @artist.save
+      flash[:success] = "#{@artist.name} added"
+      redirect_to artists_path(@artist)
+    else
+      flash.now[:error] = "Please enter all fields"
+      render :new
+    end
+  end
+
+  def show
+    
+  end
+
+  def edit
+  end
+
+  def update
+    if @artist.update(artist_params)
+      flash[:success] = "#{@artist.name} was updated"
+      redirect_to artist_path(@artist)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @artist.delete
+    flash[:success] = "#{@artist.name} was deleted"
+    redirect_to artists_path
+  end
+
+  private
+
+    def set_artist
+      @artist = Artist.find(params[:id])
+    end
+
+    def artist_params
+      params.require(:artist).permit(:name, :website, :discovered, :rating, :notes)
+    end
+
 end
