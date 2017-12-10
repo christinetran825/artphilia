082c769e075a1080686c6ec12c508f158d454354
Christine Tran <christinetran@Christines-iMac.local>
2017-12-10 00:03:12 -0800

    added associations and validations

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,11 @@
 class Artist < ApplicationRecord
+  belongs_to :user
+
+  before_validation :make_title_case
+
+  validates_presence_of :name, :website, :discovered, :rating
+
+  def make_title_case
+    self.name = self.name.titlecase
+  end
 end
