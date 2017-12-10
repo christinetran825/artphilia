683420c1fcdf4fc3535a3e3e58ea70281ba4b57d
Christine Tran <christinetran@Christines-iMac.local>
2017-12-10 11:18:36 -0800

    defined action routes

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,2 +1,57 @@
 class MediaController < ApplicationController
+
+  before_action :set_medium, only: [:show, :edit, :update, :destroy]
+  before_action :find_the_parent
+
+  def index
+    @media = Medium.all
+  end
+
+  def new
+    @medium = Medium.new
+  end
+
+  def create
+    @medium = @artwork.media.build(media_params)
+    if @medium && @medium.save
+      flash[:success] = "#{@medium.name} added"
+      redirect_to artist_path(@artist)
+    else
+      flash.now[:error] = "Please enter a medium"
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @medium.update(media_params)
+      flash[:notice] = "#{@medium.name} was updated"
+    end
+  end
+
+  def destroy
+    @medium.delete
+  end
+
+  private
+
+    def find_the_parent
+      @artwork = Artwork.find(params[:artwork_id]) #find the parent
+    end
+
+    def set_medium
+      @medium = Medium.find(params[:id])
+    end
+
+    def media_params
+      params.require(:medium).permit(:name)
+      # params.require(:media).permit(:name, artwork_ids: [])
+    end
+
+
 end
