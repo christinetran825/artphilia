c9e675d4b422709d9970859b52556422d2dbe051
Christine Tran <christinetran@Christines-iMac.local>
2017-12-10 22:55:45 -0800

    added uniqueness validation to prevent duplications

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -3,7 +3,7 @@ class Medium < ApplicationRecord
   has_many :artists, through: :artworks #medium's plural is media
 
   before_validation :make_title_case
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 
   def make_title_case
     self.name = self.name.titlecase
