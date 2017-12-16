657a3d3e45438b622aeb9ef5dfc8ef3d6bc5d31f
Christine Tran <christinetran@Christines-iMac.local>
2017-12-15 17:55:29 -0800

    defined associationes and validations

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,12 @@
 class Place < ApplicationRecord
+  belongs_to :user
+
+  validates_presence_of :fair, :gallery
+  validates :rating, presence: true, numericality: { less_than: 6 }
+
+  def make_title_case
+    self.fair = self.fair.titlecase
+    self.gallery = self.gallery.titlecase
+  end
+
 end
