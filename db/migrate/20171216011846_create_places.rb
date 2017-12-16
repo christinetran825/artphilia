1adbed9386fd68d5457014c3cede437cdb56ff40
Christine Tran <christinetran@Christines-iMac.local>
2017-12-15 23:54:02 -0800

    moved event type to its own class

--- db/migrate/20171216011846_create_places.rb
+++ db/migrate/20171216011846_create_places.rb
@@ -4,7 +4,6 @@ class CreatePlaces < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :website
       t.string :season
-      t.string :event_type
       t.integer :rating
 
       t.timestamps
