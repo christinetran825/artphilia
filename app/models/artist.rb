880cfccea0cb7f7d2b344da472491bed9b8054ba
Christine Tran <christinetran825@gmail.com>
2017-12-17 15:32:43 -0800

    added valid website regex

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,12 +3,25 @@ class Artist < ApplicationRecord
   has_many :artworks
   has_many :media, through: :artworks
 
-  before_validation :make_title_case
+  before_validation :make_title_case, :make_website
 
-  validates_presence_of :name, :website, :discovered, :rating
-  
+  validates_presence_of :name, :discovered, :rating
+
+  VALID_WEBSITE_REGEX = /(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?/
+
+  validates :website, presence: true, format: { with: VALID_WEBSITE_REGEX }
+
   def make_title_case
     self.name = self.name.titlecase
     self.discovered = self.discovered.titlecase
   end
+
+  def make_website
+    if self.website != VALID_WEBSITE_REGEX
+      self.website = "http://#{website}"
+    else
+      self.website
+    end
+  end
+
 end
