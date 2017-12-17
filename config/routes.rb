9577859eb4baf1c9023dcbb8e2308a24d078b222
Christine Tran <christinetran825@gmail.com>
2017-12-16 17:39:39 -0800

    commented user/:id path

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,14 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  get '/home', to: 'welcome#home'
+
   get '/auth/facebook/callback' => 'sessions#create'
 
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
 
   get 'users', to: 'users#index', as: 'users'
-  get 'users/:id', to: 'users#show', as: 'user'
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
