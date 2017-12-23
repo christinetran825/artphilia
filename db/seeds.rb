1d4d897f659ea21f1c3d1330f80e2796eb4583aa
Christine Tran <christinetran825@gmail.com>
2017-12-22 16:32:50 -0800

    redefined Artist objects and added artowkr and medium objects

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,42 @@ Artist.create({
 })
 
 Artist.create({
+    name: 'miss mindy',
+    website: 'www.missmindy.com',
+    discovered: 'instagram',
+    rating: 4,
+    notes: "dramatic, lots of Disney stuff",
+    user_id: 1
+})
+
+Artist.create({
+    name: 'adolie day',
+    website: 'adolieday.blogspot.com',
+    discovered: 'gallery nucleus',
+    rating: 5,
+    notes: "simple, cutesy",
+    user_id: 1
+})
+
+Artist.create({
+    name: 'fabio pastori',
+    website: 'none',
+    discovered: 'dinosaur museum in morrison, colorado',
+    rating: 5,
+    notes: "paloartist, dinosaurs, science",
+    user_id: 1
+})
+
+Artist.create({
+    name: 'maurice sendak',
+    website: 'none',
+    discovered: 'chicken soup with rice',
+    rating: 5,
+    notes: "children's illustrations, simple, funny",
+    user_id: 1
+})
+
+Artist.create({
     name: 'joey chou',
     website: 'joeyart.tumblr.com',
     discovered: 'gallery nucleus',
@@ -34,21 +70,12 @@ Artist.create({
 })
 
 Artist.create({
-    name: 'miss mindy',
-    website: 'www.etsy.com/shop/britsketch',
-    discovered: 'instagram',
-    rating: 4,
-    notes: "dramatic, lots of Disney stuff",
-    user_id: 1
-})
-
-Artist.create({
-    name: 'adolie day',
-    website: 'adolieday.blogspot.com',
-    discovered: 'gallery nucleus',
+    name: 'beatrix potter',
+    website: 'www.peterrabbit.com',
+    discovered: 'peter rabbit storybook, local library',
     rating: 5,
-    notes: "simple, cutesy",
-    user_id: 1
+    notes: "children's illustration, creatures, child-like awe, mushrooms, Scientific Studies",
+    user_id: 2
 })
 
 Artist.create({
@@ -60,29 +87,100 @@ Artist.create({
     user_id: 3
 })
 
-Artist.create({
-    name: 'fabio pastori',
-    website: 'none',
-    discovered: 'dinosaur museum in morrison, colorado',
+Artwork.create({
+    title: 'Blood, Sweat, Tears',
+    exhibition: 'gallery one',
+    user_owned: 'Yes',
+    signed: "Yes",
+    original: "Original",
+    comments: "test",
     rating: 5,
-    notes: "paloartist, dinosaurs, science",
-    user_id: 1
+    artist_id: 1,
+    medium_id: 1
 })
+# medium = oil
 
-Artist.create({
-    name: 'beatrix potter',
-    website: 'www.peterrabbit.com',
-    discovered: 'peter rabbit storybook, local library',
+Artwork.create({
+    title: 'good luck',
+    exhibition: 'gallery two',
+    user_owned: 'No',
+    signed: nil,
+    original: nil,
+    comments: "test",
     rating: 5,
-    notes: "children's illustration, creatures, child-like awe, mushrooms, Scientific Studies",
-    user_id: 2
+    artist_id: 1,
+    medium_id: 2
 })
+# medium = acrylic
 
-Artist.create({
-    name: 'maurice sendak',
-    website: 'none',
-    discovered: 'chicken soup with rice',
-    rating: 5,
-    notes: "children's illustrations, simple, funny",
-    user_id: 1
+Artwork.create({
+    title: 'the hospital',
+    exhibition: 'the art of whipped cream',
+    user_owned: 'No',
+    signed: nil,
+    original: nil,
+    comments: "test",
+    rating: 3,
+    artist_id: 1,
+    medium_id: 3
 })
+# medium = graphite
+
+Artwork.create({
+    title: 'christina',
+    exhibition: 'wondertoonel',
+    user_owned: 'No',
+    signed: nil,
+    original: nil,
+    comments: "test",
+    rating: 5,
+    artist_id: 1,
+    medium_id: 1
+})
+# medium = oil
+
+Artwork.create({
+    title: 'The Creatrix',
+    exhibition: 'wondertoonel',
+    user_owned: 'No',
+    signed: nil,
+    original: nil,
+    comments: "test",
+    rating: 5,
+    artist_id: 1,
+    medium_id: 1
+})
+# medium = oil
+
+Artwork.create({
+    title: 'Anatomia',
+    exhibition: 'porcelains',
+    user_owned: 'No',
+    signed: nil,
+    original: nil,
+    comments: "test",
+    rating: 5,
+    artist_id: 1,
+    medium_id: 4
+})
+# medium = porcelain
+
+Medium.create({
+  name: "oil",
+  artwork_id: [1, 4, 5]
+  })
+
+Medium.create({
+  name: "acrylic",
+  artwork_id: 2
+  })
+
+Medium.create({
+  name: "graphite",
+  artwork_id: 3
+  })
+
+Medium.create({
+  name: "porcelain",
+  artwork_id: 6
+  })
