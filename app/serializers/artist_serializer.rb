2a40b51e4fd8ac0768561404101c3b593f35fec5
github username <github email address>
2018-02-28 13:37:39 -0800

    removed first and last name to just name

--- app/serializers/artist_serializer.rb
+++ app/serializers/artist_serializer.rb
@@ -1,5 +1,5 @@
 class ArtistSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :website, :discovered, :rating, :notes, :user_id, :artworks
+  attributes :id, :name, :website, :discovered, :rating, :notes, :user_id, :artworks
   # belongs_to :user
 
   private
