245bc11762cd0b80aa9bfca5fce39bf6891b2599
github username <github email address>
2018-02-28 12:03:21 -0800

    updated Option A getNewArtwork

--- app/assets/javascripts/artworks.js
+++ app/assets/javascripts/artworks.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   getNewArtwork();
-  getEditArtwork();
+  // getEditArtwork();
 })
 
 // OPTION A
@@ -11,33 +11,47 @@ const getNewArtwork = () => {
     $.get(this.href).success(function(response){
       let _template = response
       let template = $.parseHTML(_template)
-      let heading = `<h1>Add a New Artwork</h1>`
-      let links = cancelToArtistArtworks(id)
+      let theHeader = $(template).find(".header")
       let theForm = $(template).find("#new_artwork")
-      theArtworkNewEditBody(heading, links);
-      $(".content").html(theForm);
+      theNewEditBody(theHeader, theForm);
       addOwnershipForm(theForm);
     })
   })
 }
 
-//Artworks Index is in Artist Show within Show All Artwork button event
-const getEditArtwork = () => {
-  $(document).on('click', "#update_artwork", function(e){
+function cancelFromNewArtwork(){
+  $(document).on('click', ".artist_show_link", function(e){
     e.preventDefault();
+    console.log("hello")
     // let id = $(this).attr("data-id");
-    $.get(this.href).success(function(response){
-      let _template = response
-      let template = $.parseHTML(_template)
-      let heading = $(template).find(".header")
-      // let links = cancelToArtistArtworks(id)
-      let theForm = $(template).find(".edit_artwork")
-      theNewEditBody(heading, theForm)
-      // $(".content").html(theForm);
-      addOwnershipForm(theForm);
-    })
-  });
+    // $.get(this.href).success(function(response){
+    //   let _template = response
+    //   let template = $.parseHTML(_template)
+    //   let theHeader = $(template).find(".header")
+    //   let theForm = $(template).find("#new_artwork")
+    //   theNewEditBody(theHeader, theForm);
+    //   addOwnershipForm(theForm);
+    // })
+  })
 }
+
+//Artworks Index is in Artist Show within Show All Artwork button event
+// const getEditArtwork = () => {
+//   $(document).on('click', "#update_artwork", function(e){
+//     e.preventDefault();
+//     // let id = $(this).attr("data-id");
+//     $.get(this.href).success(function(response){
+//       let _template = response
+//       let template = $.parseHTML(_template)
+//       let heading = $(template).find(".header")
+//       // let links = cancelToArtistArtworks(id)
+//       let theForm = $(template).find(".edit_artwork")
+//       theNewEditBody(heading, theForm)
+//       // $(".content").html(theForm);
+//       addOwnershipForm(theForm);
+//     })
+//   });
+// }
 
 // function cancelToArtistArtworks(theClickedArtist){
 //   let backTheArtist = `<button><a href="/artists/${theClickedArtist}/artworks" data-id="${theClickedArtist}" class="artist_show_link">Cancel</a></button>`
@@ -68,13 +82,13 @@ const getEditArtwork = () => {
 //   return backTheArtist
 // }
 
-// Artwork new - form
-// function addOwnershipForm(ownArtworkSection) {
-//   $("#artwork_user_owned_yes").click(function(){
-//     if ((this.value) = "Yes") {
-//       $("div:hidden").show("fast")
-//     } else {
-//       $("div:hidden").hide("fast")
-//     }
-//   })
-// }
+// Artwork ownership portion - form
+function addOwnershipForm(ownArtworkSection) {
+  $("#artwork_user_owned_yes").click(function(){
+    if ((this.value) = "Yes") {
+      $("div:hidden").show("fast")
+    } else {
+      $("div:hidden").hide("fast")
+    }
+  })
+}
