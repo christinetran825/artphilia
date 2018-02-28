5546f109c11bd3d84463d0ea1f092381952439d3
github username <github email address>
2018-02-27 21:30:14 -0800

    added artwork constructor and prototype

--- app/assets/javascripts/prototypes.js
+++ app/assets/javascripts/prototypes.js
@@ -9,6 +9,20 @@ function Artist(artist){
   this.notes = artist.notes
 }
 
+function Artwork(artwork){
+  this.id = artwork.id,
+  this.title = artwork.title,
+  this.exhibition = artwork.exhibition,
+  this.user_owned = artwork.user_owned,
+  this.comments = artwork.comments,
+  this.rating = artwork.rating
+}
+
+function Medium(medium){
+  this.id = medium.id,
+  this.name = medium.name
+}
+
 /////// ALL ARTISTS ///////
 Artist.prototype.formatArtistIndexData = function(numofArtworks) {
   let artistHtml = `
@@ -37,4 +51,19 @@ Artist.prototype.formatFavArtistIndexData = function() {
     </tr>
   `
   return favArtists
+}
+
+Artwork.prototype.formatArtworkOwnedData = function(theOwnedArtworkArtist) {
+  // let theOwnedArtworkArtist = `${this.first_name} ${this.last_name}`;
+  let artworkOwned = `
+    <tr>
+      <td><a href="/artists/${theOwnedArtworkArtist}/artworks/${this.id}">images</a></td>
+      <td><a href="/artists/${theOwnedArtworkArtist}/artworks/${this.id}">${this.title}</a></td>
+      <td><a href="/artists/${theOwnedArtworkArtist}/artworks/${this.id}">${this.exhibition}</a></td>
+      <td><a href="/artists/${theOwnedArtworkArtist}">${theOwnedArtworkArtist}</a></td>
+      <td><a href="/artists/${theOwnedArtworkArtist}/artworks/${this.id}" class="update_artwork">Edit</a></td>
+      <td><a href="/artists/${theOwnedArtworkArtist}/artworks/${this.id}" data-method="delete">Delete</a></td>
+    </tr>
+  `
+  return artworkOwned
 }
