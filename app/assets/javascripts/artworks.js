b6b677d0e73b4b91dc5ace61c75251828baf6ecd
github username <github email address>
2018-02-28 09:15:26 -0800

    updated edit artwork

--- app/assets/javascripts/artworks.js
+++ app/assets/javascripts/artworks.js
@@ -1,70 +1,80 @@
 $(document).ready(function() {
-  addNewArtwork();
-  artworkEdit();
+  getNewArtwork();
+  getEditArtwork();
 })
 
-//Artworks Index is in Artist Show within Show All Artwork button event
-const artworkEdit = () => {
-  $(document).on('click', ".update_artwork", function(e){
-    e.preventDefault();
-  });
-}
-
 // OPTION A
-// const addNewArtwork = () => {
-//   $(document).on('click', "a#new_artwork", function(e){
-//     e.preventDefault();
-//     let id = $(this).attr("data-id");
-//     $.get(this.href).success(function(response){
-//       let _template = response
-//       let template = $.parseHTML(_template)
-//       let final = $(template).find("#artwork_form")
-//       addOwnershipForm(final)
-//       artworkNewEdit(id, final);
-//     })
-//   })
-// }
-// function artworkNewEdit(theID, theForm){
-//   let objHeaderDetails = `<h1>New Artwork</h1>`
-//   let objButtons = cancelToTheArtist(theID);
-//   let objPageContent = theForm;
-//   objectNewEdit(objHeaderDetails, objButtons, objPageContent);
-// }
-// function cancelToTheArtist(theClickedArtist){
-//   let backAllArtists = `<button><a href="/artists/${theClickedArtist}" data-id="${theClickedArtist}" class="artist_show_link">Cancel</a></button>`
-//   return backAllArtists
-// }
-
-// Option B (formally Option C)
-const addNewArtwork = () => {
+const getNewArtwork = () => {
   $(document).on('click', "#add_artwork", function(e){
     e.preventDefault();
-    // don't repaint the DOM; add the artwork form in pageContent
     let id = $(this).attr("data-id");
     $.get(this.href).success(function(response){
       let _template = response
       let template = $.parseHTML(_template)
-      let cancel = $(template).find("#the_artworks .obj-links")
-      let final = $(template).find("#new_artwork")
-      theShowArtworksPortion();
-      $("#mini.obj-links").html(cancel)
-      $("#mini.content").html(final);
-      addOwnershipForm(final);
+      let heading = `<h1>Add a New Artwork</h1>`
+      let links = cancelToArtistArtworks(id)
+      let theForm = $(template).find("#new_artwork")
+      theArtworkNewEditBody(heading, links);
+      $(".content").html(theForm);
+      addOwnershipForm(theForm);
     })
   })
 }
-function cancelToArtistArtworks(theClickedArtist){
-  let backAllArtists = `<button><a href="/artists/${theClickedArtist}/artworks" data-id="${theClickedArtist}" id="load_artworks">Cancel</a></button>`
-  return backAllArtists
+
+//Artworks Index is in Artist Show within Show All Artwork button event
+const getEditArtwork = () => {
+  $(document).on('click', "#update_artwork", function(e){
+    e.preventDefault();
+    // let id = $(this).attr("data-id");
+    $.get(this.href).success(function(response){
+      let _template = response
+      let template = $.parseHTML(_template)
+      let heading = $(template).find(".header")
+      // let links = cancelToArtistArtworks(id)
+      let theForm = $(template).find(".edit_artwork")
+      theNewEditBody(heading, theForm)
+      // $(".content").html(theForm);
+      addOwnershipForm(theForm);
+    })
+  });
 }
 
+// function cancelToArtistArtworks(theClickedArtist){
+//   let backTheArtist = `<button><a href="/artists/${theClickedArtist}/artworks" data-id="${theClickedArtist}" class="artist_show_link">Cancel</a></button>`
+//   return backTheArtist
+// }
+
+// Option B (formally Option C)
+// const getNewArtwork = () => {
+//   $(document).on('click', "#add_artwork", function(e){
+//     e.preventDefault();
+//     // don't repaint the DOM; add the artwork form in pageContent
+//     let id = $(this).attr("data-id");
+//     $.get(this.href).success(function(response){
+//       let _template = response
+//       let template = $.parseHTML(_template)
+//       // let cancel = $(template).find("#the_artworks .obj-links")
+//       let final = $(template).find("#new_artwork")
+//       theShowArtworksPortion();
+//       $("#mini .obj-links").html(cancelToArtistArtworks(id))
+//       $("#mini .content").html(final);
+//       addOwnershipForm(final);
+//     })
+//   })
+// }
+
+// function cancelToArtistArtworks(theClickedArtist){
+//   let backTheArtist = `<button><a href="/artists/${theClickedArtist}/artworks" data-id="${theClickedArtist}" id="load_artworks">Cancel</a></button>`
+//   return backTheArtist
+// }
+
 // Artwork new - form
-function addOwnershipForm(ownArtworkSection) {
-  $("#artwork_user_owned_yes").click(function(){
-    if ((this.value) = "Yes") {
-      $("div:hidden").show("fast")
-    } else {
-      $("div:hidden").hide("fast")
-    }
-  })
-}
+// function addOwnershipForm(ownArtworkSection) {
+//   $("#artwork_user_owned_yes").click(function(){
+//     if ((this.value) = "Yes") {
+//       $("div:hidden").show("fast")
+//     } else {
+//       $("div:hidden").hide("fast")
+//     }
+//   })
+// }
