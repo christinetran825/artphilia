71a3a19ab3341fbb41fd697fe020b82a8ae4ec87
github username <github email address>
2018-02-27 21:29:17 -0800

    added owned artwork functions; still need to resolve table data collection

---
+++ app/assets/javascripts/own_artworks.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+  artworksOwned(); // .../artists
+})
+
+/////// click on nav bar link for owned Artwork ///////
+const artworksOwned = () => {
+  $('#owned_artworks').on('click', function(e){
+    e.preventDefault();
+    $.get(this.href).success(function(response){
+      let _template = response
+      let template = $.parseHTML(_template)
+      let theHeader = $(template).find(".header")
+      let theTableHeader= $(template).find(".table thead tr")
+      theIndexBody(theHeader, theTableHeader)
+    });
+    addArtworksOwnedByYes(this)
+  });
+}
+
+/////// DEFINE ARTWORK Filter for owned artwork ///////
+function filterOwnership(collection, cb){
+  const artworksHave = [];
+  for (const element of collection){
+    if (cb(element)){
+      artworksHave.push(element);
+    }
+  }
+  return artworksHave;
+}
+
+/////// ADD OWNED ARTWORKS using filter function ///////
+function addArtworksOwnedByYes(allArtworks) {
+  fetch(`/artists.json`)
+  .then(res => res.json())
+  .then(theArtists => {
+    theArtists.forEach(artist => {
+     debugger;
+     let theArtworks = artist.artworks
+     const whichArtworkOwned = filterOwnership(theArtworks, function(artworks){
+       return artworks.user_owned === "Yes"
+     });
+     debugger;
+     whichArtworkOwned.forEach(artwork => {
+       let theOwnedArtwork = new Artwork(artwork); //create newArtwork
+       let theOwnedArtworkArtist = artwork.artist_id
+       let eachOwnedArtwork = theOwnedArtwork.formatArtworkOwnedData(theOwnedArtworkArtist);
+       tableContent(eachOwnedArtwork)
+     });
+    })
+  })
+}
