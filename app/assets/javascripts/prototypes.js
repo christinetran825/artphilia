62be30fc273cfb5bd673b8cb828455a65315d7ac
github username <github email address>
2018-03-06 14:57:27 -0800

    removed <tr> from ArtistIndexData

--- app/assets/javascripts/prototypes.js
+++ app/assets/javascripts/prototypes.js
@@ -26,14 +26,12 @@ function Medium(medium){
 /////// ALL ARTISTS ///////
 Artist.prototype.formatArtistIndexData = function(numofArtworks) {
   let artistHtml = `
-    <tr>
       <td><a href="/artists/${this.id}" data-id="${this.id}" class="artist_show_link">${this.name}</a></td>
       <td><a href="/artists/${this.id}" data-id="${this.id}" class="artist_show_link">${numofArtworks}</a></td>
       <td><a target=_blank href="${this.website}">${this.website}</a></td>
       <td>${this.rating}</td>
       <td><a href="/artists/${this.id}/edit" data-id="${this.id}" id="update_artist">Edit</a></td>
       <td><a href="/artists/${this.id}" id="delete_artist" data-method="delete">Delete</a></td>
-    </tr>
   `
   return artistHtml
 }
